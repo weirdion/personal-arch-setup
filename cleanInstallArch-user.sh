ebb74d6c25c56cc07ff7ac5bf5e7e52ea9bc9d1f
Ankit Sadana <sadana.ankit@gmail.com>
2018-05-25 21:44:25 -0400

    Updated username. Added noconfirm tag for pacaur

--- cleanInstallArch-user.sh
+++ cleanInstallArch-user.sh
@@ -6,13 +6,13 @@
 #
 # Created By: Ankit Sadana
 # Created On: 02/13/2018
-# Last Edited On: 02/13/2018
+# Last Edited On: 05/25/2018
 
-declare -r scriptUser="ankit"
+declare -r scriptUser="asadana"
 declare -r storeMountDir="/run/media/$scriptUser/store"
 declare -r homeDir="/home/$scriptUser"
 
-createSoftLinks() {	
+createSoftLinks() {
 	echo
 	read -r -p "Do you want to delete \"Documents\", \"Pictures\" and \"Downloads\"  before creating soft links? [y/n]: " response
 	response=${response,,}    # to lower
@@ -42,40 +42,43 @@ createSoftLinks() {
 	ln -s "$storeMountDir/wallpapers" "$homeDir/wallpapers" && \
 	ln -s "$storeMountDir/workspace" "$homeDir/workspace" && \
 	ln -s "$storeMountDir/android-sdk" "$homeDir/android-sdk"
+	cp -rv "$storeMountDir/.AndroidStudio3.0" "$homeDir/.AndroidStudio3.0"
+	cp -rv "$storeMountDir/.IdeaIC2018.1" "$homeDir/.IdeaIC2018.1"
 
 	echo
 	echo "Soft links created"
-	ls -al --color=auto $homeDir
+	ls -ahl --color=auto $homeDir
 }
 
 copyConfigFilesFromBackup() {
 	echo
-	read -r -p "Do you want to copy/replace bashrc and stuff? [y/n]: " response2
+	read -r -p "Do you want to delete existing bashrc and stuff and restore from backup? [y/n]: " response2
 	response2=${response2,,}
 	echo
 	if [[ "$response2" =~ ^(yes|y)$ ]]; then
-		# rm -v $homeDir/.bashrc
+		rm -v $homeDir/.bashrc
 		rm -v $homeDir/.face
 		rm -v $homeDir/.face.icon
 		rm -v $homeDir/.gitconfig
 		rm -rfv $homeDir/.m2
 		rm -rfv $homeDir/.ssh
-		# cp -v $storeMountDir/backup/.bashrc $homeDir/
+		cp -v $storeMountDir/backup/.bashrc $homeDir/
 		cp -v $storeMountDir/backup/.face $homeDir/ && \
-		cp -v $storeMountDir/backup/.face.icon $homeDir/ && \
 		cp -v $storeMountDir/backup/.gitconfig $homeDir/ && \
 		cp -rv $storeMountDir/backup/.config/kfoldersync $homeDir/.config/ && \
 		cp -rv $storeMountDir/backup/.m2 $homeDir/.m2 && \
 		cp -rv $storeMountDir/backup/.ssh $homeDir/.ssh
+		ln -s $homeDir/.face $homeDir/.face.icon
 	fi
 	echo
 }
 
 installApplicationsWithPacaur() {
-	pacaur -S jdk8 vim ntfs-3g	
-	pacaur -S tilix-bin plex-media-server remmina remmina-plugin-rdesktop insync firefox-beta-bin \
-	google-chrome-beta git evolution qbittorrent openvpn networkmanager-openvpn openssh sublime-text-dev freerdp \
-	intellij-idea-community-edition firefox-beta-bin powertop vlc private-internet-access-vpn python pip android-studio-beta p7zip
+	pacaur -S --noconfirm jdk8-openjdk vim ntfs-3g
+	pacaur -S --noconfirm tilix-bin plex-media-server remmina remmina-plugin-rdesktop insync xkeychain maven \
+	google-chrome-beta git evolution qbittorrent openvpn networkmanager-openvpn openssh sublime-text-dev freerdp gradle \
+	powertop vlc python p7zip
+	pacaur -S --noconfirm telegram-desktop-bin slack-desktop discord
 }
 
 # Check if script is running as root
